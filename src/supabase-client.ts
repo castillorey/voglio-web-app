e574f25ffec072fae35c7ea332310501634d5b7b
Kevin Castillo <castilloreyeskm@gmail.com>
2025-09-30 19:52:32 -0500

    Change API keys to point to new Supabase project

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://bblscslptefmqyjhizvl.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJibHNjc2xwdGVmbXF5amhpenZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY2MTI5NzQsImV4cCI6MjA1MjE4ODk3NH0.9Q463B1wGmM8fv7EZwr7Jx3U_5rJrqAAo6_2ZbmCFgE";
+const supabaseUrl = "https://utmwcxmirkbgshowswyz.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0bXdjeG1pcmtiZ3Nob3dzd3l6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MzAwMDIsImV4cCI6MjA3NDMwNjAwMn0.gzQoCtcLBrgHki3rxVnAYnoLqi111fqUYS5kmVet-a0";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
