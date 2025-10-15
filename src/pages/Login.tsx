96d9e6874927e9332869d00fcf40de6e99af959e
Kevin Castillo <castilloreyeskm@gmail.com>
2025-10-15 17:22:03 -0500

    Change Login UI

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,17 @@
 import { SyntheticEvent, useState } from "react";
 import supabase from "../supabase-client";
 import { Link, useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 function login() {
   const navigate = useNavigate();
@@ -31,83 +42,69 @@ function login() {
 
   return (
     <>
-      <div className="flex justify-center px-6 py-12 lg:px-8">
-        <div className="max-w-md w-full rounded-xl shadow-lg p-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 className="mt-5 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-              Login
-            </h2>
+    <div className="flex justify-center py-12 lg:px-8">
+      <Card className="w-full max-w-sm content-center">
+      <CardHeader>
+        <CardTitle>Login to your account</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="m@example.com"
+                required
+                autoComplete="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                {/* <a
+                  href="#"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Forgot your password?
+                </a> */}
+              </div>
+              <Input 
+                id="password" 
+                type="password" 
+                required 
+                autoComplete="current-password"
+                onChange={(e) => setpassword(e.target.value)}
+                value={password}/>
+            </div>
           </div>
-
-          <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form onSubmit={handleSubmit} method="POST" className="space-y-3">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm/6 font-medium text-gray-900"
-                >
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                    id="email"
-                    name="email"
-                    type="email"
-                    required
-                    autoComplete="email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                  />
-                </div>
-              </div>
-
-              <div>
-                <div className="flex items-center justify-between">
-                  <label
-                    htmlFor="password"
-                    className="block text-sm/6 font-medium text-gray-900"
-                  >
-                    Password
-                  </label>
-                </div>
-                <div className="mt-2">
-                  <input
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    autoComplete="current-password"
-                    onChange={(e) => setpassword(e.target.value)}
-                    value={password}
-                  />
-                </div>
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Log In
-                </button>
-                {message && <p className="mt-2">{message}</p>}
-              </div>
-            </form>
-
-            <p className="mt-5 text-center text-sm/6 text-gray-500">
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+        {message && <p className="mt-2">{message}</p>}
+        <p className="mt-5 text-center text-sm/6 text-gray-500">
               Don't have an account?{" "}
               <Link
                 to={"/register"}
                 className="font-semibold text-indigo-600 hover:text-indigo-500"
               >
-                Sign in
+                Sign up 
               </Link>
             </p>
-          </div>
-        </div>
-      </div>
+      </CardFooter>
+    </Card>
+    </div>
+    
     </>
   );
 }
