bbabb34eff898a59f1b9532b38eda2eab73ce3c7
Kevin Castillo <castilloreyeskm@gmail.com>
2026-01-06 11:48:36 -0500

    Add loading spinner on Auth pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,9 @@
 import { SyntheticEvent, useState } from "react";
 import supabase from "../supabase-client";
 import { Link, useNavigate } from "react-router-dom";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner"
+
 import {
   Card,
   CardContent,
@@ -9,25 +11,28 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 function login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState<string>("");
   const [password, setpassword] = useState<string>("");
   const [message, setMessage] = useState<string>("");
+  const [loading, setloading] = useState<boolean>(false);
+  
 
   const handleSubmit = async (event: SyntheticEvent): Promise<void> => {
     event.preventDefault();
     setMessage("");
+    setloading(true);
 
     const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password,
     });
-
+    setloading(false);
     if (error) {
       setMessage(error.message);
       setEmail("");
@@ -41,71 +46,70 @@ function login() {
   };
 
   return (
-    <>
-      <div className="flex justify-center py-12 lg:px-8">
-        <Card className="w-full max-w-sm content-center">
-          <CardHeader>
-            <CardTitle>Login to your account</CardTitle>
-            <CardDescription>
-              Enter your email below to login to your account
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form>
-              <div className="flex flex-col gap-6">
-                <div className="grid gap-2">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    name="email"
-                    placeholder="m@example.com"
-                    required
-                    autoComplete="email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                  />
-                </div>
-                <div className="grid gap-2">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    {/* <a
+    <div className="flex justify-center py-12 lg:px-8">
+      <Card className="w-full max-w-sm content-center">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="m@example.com"
+                  required
+                  autoComplete="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  {/* <a
                   href="#"
                   className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
                 >
                   Forgot your password?
                 </a> */}
-                  </div>
-                  <Input
-                    id="password"
-                    type="password"
-                    required
-                    autoComplete="current-password"
-                    onChange={(e) => setpassword(e.target.value)}
-                    value={password} />
                 </div>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  onChange={(e) => setpassword(e.target.value)}
+                  value={password}
+                />
               </div>
-            </form>
-          </CardContent>
-          <CardFooter className="flex-col gap-2">
-            <Button type="button" className="w-full" onClick={handleSubmit}>
-              Login
-            </Button>
-            {message && <p className="mt-2">{message}</p>}
-            <p className="mt-5 text-center text-sm/6 text-gray-500">
-              Don't have an account?{" "}
-              <Link
-                to={"/register"}
-                className="font-semibold text-indigo-600 hover:text-indigo-500"
-              >
-                Sign up
-              </Link>
-            </p>
-          </CardFooter>
-        </Card>
-      </div>
-
-    </>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="button" className="w-full" onClick={handleSubmit}>
+            {loading && <Spinner/>}
+            Login
+          </Button>
+          {message && <p className="mt-2 text-red-500">{message}</p>}
+          <p className="mt-5 text-center text-sm/6 text-gray-500">
+            Don't have an account?{" "}
+            <Link
+              to={"/register"}
+              className="font-semibold text-indigo-600 hover:text-indigo-500"
+            >
+              Sign up
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
 
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,21 +1,35 @@
-import React, { SyntheticEvent, useState } from "react";
-import { Link } from "react-router-dom";
+import { SyntheticEvent, useState } from "react";
 import supabase from "../supabase-client";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 function register() {
   const [email, setEmail] = useState<string>("");
   const [password, setpassword] = useState<string>("");
   const [message, setMessage] = useState<string>("");
+  const [loading, setloading] = useState<boolean>(false);
 
   const handleSubmit = async (event: SyntheticEvent): Promise<void> => {
     event.preventDefault();
     setMessage("");
+    setloading(true);
 
     const { data, error } = await supabase.auth.signUp({
       email,
       password,
     });
-
+    setloading(false);
     if (error) {
       setMessage(error.message);
       return;
@@ -27,52 +41,36 @@ function register() {
     setpassword("");
   };
   return (
-    <>
-    <div className="flex justify-center px-6 py-12 lg:px-8">
-      <div className="max-w-md w-full rounded-xl shadow-lg p-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-5 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-            Register
-          </h2>
-        </div>
-
-        <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form onSubmit={handleSubmit} method="POST" className="space-y-3">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+    <div className="flex justify-center py-12 lg:px-8">
+      <Card className="w-full max-w-sm content-center">
+        <CardHeader>
+          <CardTitle>Create new account</CardTitle>
+          <CardDescription>
+            Enter your email below to create your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
                   id="email"
-                  name="email"
                   type="email"
+                  name="email"
+                  placeholder="m@example.com"
                   required
                   autoComplete="email"
                   onChange={(e) => setEmail(e.target.value)}
                   value={email}
                 />
               </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm/6 font-medium text-gray-900"
-                >
-                  Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input
                   id="password"
-                  name="password"
                   type="password"
                   required
                   autoComplete="current-password"
@@ -81,18 +79,14 @@ function register() {
                 />
               </div>
             </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Create account
-              </button>
-              {message && <p className="mt-2">{message}</p>}
-            </div>
           </form>
-
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="button" variant="secondary" className="w-full" onClick={handleSubmit}>
+            {loading && <Spinner/>}
+            Create account
+          </Button>
+          {message && <p className="mt-2 ">{message}</p>}
           <p className="mt-5 text-center text-sm/6 text-gray-500">
             Already a member?{" "}
             <Link
@@ -102,10 +96,9 @@ function register() {
               Log in
             </Link>
           </p>
-        </div>
-      </div>
+        </CardFooter>
+      </Card>
     </div>
-    </>
   );
 }
 
